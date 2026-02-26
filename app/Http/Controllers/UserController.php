cb024a72d38158f70e286535109d2b9d2f68d8db
Ives <ivesmvisagie@gmail.com>
2026-02-26 15:00:34 +0200

    Added Some Comments

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,26 +14,33 @@ class UserController extends Controller
     public function store(Request $request)
     {
         try{
+
+            //Validatating the  Data
             $validated = $request->validate([
                 'name' => ['required', 'string', 'max:255'],
                 'email' => ['required', 'email', 'max:255', 'unique:users,email'],
                 'additionalcomment' => ['required', 'string', 'max:255'],
             ]);
 
+            //Commiting to the  db
             User::create($validated);
 
            return redirect()->route('confirmation.page')->with('message', 'Action completed successfully!');
         } catch (QueryException $e) {
 
+            // Some error handling
+
             if ($e->getCode() == 23000) {
+                //Duplicate Email
                 return redirect()->route('error.page')
                     ->with('error', 'Email address already exists.');
             }
 
+            //Somethin catestrophic went wrong
             return redirect()->route('error.page')
                 ->with('error', 'Something went wrong.');
             }
-        
+         
     }
 
 }
